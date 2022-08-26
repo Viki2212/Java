f72e9694d6039a85f8a6b9f719cb1a9755f873e9
Viki2212 <111008143+Viki2212@users.noreply.github.com>
2022-08-26 20:18:41 +0530

    'Firstday'

---
+++ FirstDay.java
@@ -0,0 +1,26 @@
+public class FirstDay {
+
+    public static void main(String[] args){
+        String name = "Viki";
+        char initial = 's';
+        byte age = 28;
+        float height = 168.5F;
+        double weight = 88.5555;
+        short salary = 10000;
+        long phoneNo = 9629017523L;
+        int pincode = 636005;
+        boolean like = true;
+
+        System.out.println(name);
+        System.out.println(initial);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(salary);
+        System.out.println(phoneNo);
+        System.out.println(pincode);
+        System.out.println(like);
+
+    }
+
+}
