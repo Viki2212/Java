2292e9f7c3595c84a88c08c221cfc56454ca269c
Viki2212 <111008143+Viki2212@users.noreply.github.com>
2022-08-26 20:24:23 +0530

    'update'

---
+++ FourDay.java
@@ -0,0 +1,12 @@
+public class FourDay {
+    public void calling(String name){
+        System.out.println("Calling to " + name);
+    }
+    public void calling(int number){
+        System.out.println("Calling to " + number);
+    }
+    public int calling(int a, int b){
+        int num3 = a + b;
+        return num3;
+    }
+}
