64b0d21f8f45def5e8e592767f76cd155b6e1f8c
Viki2212 <111008143+Viki2212@users.noreply.github.com>
2022-08-26 20:25:50 +0530

    'second'

---
+++ SecondDay.java
@@ -0,0 +1,24 @@
+class ThirdDay {
+    public String CarModel() {
+        String Model = "Swift";
+        String Brand = "Maruthi";
+        return Model +" "+ Brand;
+    }
+    public boolean CarCondition() {
+        boolean Damage = true;
+        boolean insurance = false;
+        return true;
+    }
+    public void CarPrice() {
+        int OnRoad = 140000;
+        int tax = 15000;
+        System.out.println(OnRoad);
+        System.out.println(tax);
+    }
+    public int OrgPrice(){
+        int Price = 145000;
+        /*System.out.println(Price);*/
+        return Price;
+
+    }
+}
