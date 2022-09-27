bf1b8a8f49d1b7d711f2a33def0e58ea1880bdef
Viki2212 <viigneshsekar@gmail.com>
2022-09-27 21:53:56 +0530

    27-09-2022

--- learnSelenium/LanchChrome.java
+++ learnSelenium/LanchChrome.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LanchChrome {
 
 	public static void main(String[] args) {
-		
+
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://letcode.in/edit");
+	
 	}
 }
