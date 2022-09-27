e49c6f2e76a829a827121f574012f940083c2975
Viki2212 <viigneshsekar@gmail.com>
2022-09-27 13:53:16 +0530

    27.09.2022

--- thirdDay/Recap.java
+++ thirdDay/Recap.java
@@ -2,33 +2,33 @@ package thirdDay;
 
 public class Recap {
 	
-	public Recap(){		//Constructor
+	public Recap(){							//Constructor
 		this.c = 230;
 		this.e = 20;
 	}
 	
-	int c; //instance Variable / Global variable
+	int c; 									//instance Variable / Global variable
 	int e;
-	static int g = 20; // Static Variable
+	static int g = 20; 						// Static Variable
 	
-	public static int remain() { //Static Method
+	public static int remain() { 			//Static Method
 		int f = 20;
 		int h = f + g;
 		System.out.println(h);
 		return h;
 	}
 	
-	public int amount(int c) { //Non-Static method
+	public int amount(int c) { 				//Non-Static method
 		int a = 100;
 		int b = 200;
-		int d = a+b+this.c+c+g;
+		int d = a+b+this.c+c+g+e;
 		System.out.println(d);
 		return d;
 	}
 
 	public static void main(String[] args) { //Main method
 		Recap rp = new Recap();
-		rp.amount(20); // Non-Static method calling
-		remain(); // Static method calling
+		rp.amount(20); 						// Non-Static method calling
+		remain(); 							// Static method calling
 	}
 }
