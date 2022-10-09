917ab03baf71070638eb8b53062b2b09b557cd70
Viki2212 <viigneshsekar@gmail.com>
2022-10-10 01:56:48 +0530

    10/10/2022

--- learnSelenium/Test.java
+++ learnSelenium/Test.java
@@ -1,38 +1,7 @@
 package learnSelenium;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.io.FileHandler;
-
 public class Test {
 
-	public static void main(String[] args) throws IOException, InterruptedException  {
-		
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		driver.findElementByXPath("//button[.='✕']").click();
-		driver.findElementByXPath("//div[.='Electronics']").click();
-		WebElement target = driver.findElementByXPath("//span[.='Electronics']");
-		Actions builder = new Actions(driver);
-		builder.moveToElement(target).perform();
-		driver.findElementByLinkText("Mi").click();
-		Thread.sleep(3000);
-		
-		File src = driver.getScreenshotAs(OutputType.FILE);
-		File dest = new File("./Snaps/img.png");
-		FileHandler.copy(src, dest);
-
-	}
+	
 
 }
